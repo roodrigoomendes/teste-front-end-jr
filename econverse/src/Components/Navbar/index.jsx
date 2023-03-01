92f49c1aff8080cc7713ac7bb33fd5831495c0bc
Rodrigo Mendes <roodrigoomendessilva@gmail.com>
2023-03-01 20:34:24 -0300

    creating part middle of navbar

--- econverse/src/Components/Navbar/index.jsx
+++ econverse/src/Components/Navbar/index.jsx
@@ -1,7 +1,12 @@
 import shieldCheck from '../../img/shieldCheck.png'
 import truck from '../../img/truck.png'
 import creditCard from '../../img/CreditCard.png'
-
+import vtex from '../../img/vtex.png'
+import shoppingCart from '../../img/ShoppingCart.png'
+import heart from '../../img/Heart.png'
+import usercircle from '../../img/UserCircle.png'
+import order from '../../img/order.png'
+import magnifyingGlass from '../../img/MagnifyingGlass.png'
 
 import './style.sass'
 
@@ -25,7 +30,29 @@ export function Navbar(){
 
       <div className='middle'>
 
-      </div>
+        <img src={vtex} alt="" />
+        
+        <div className='box-search'>
+          <input type="search" placeholder='O que você está buscando?' name="search" id="search" />
+          <img src={magnifyingGlass} alt="" />
+        </div>
+
+        <div className='box-shortcuts'>
+          <a href="">
+            <img src={order} alt="" />
+          </a>
+          <a href="">
+            <img src={heart} alt="" />
+          </a>
+          <a href="">
+            <img src={usercircle} alt="" />
+          </a>
+          <a href="">
+            <img src={shoppingCart} alt="" />
+          </a>
+        </div>
+      
+      </div> 
     </nav>
   )
 }
