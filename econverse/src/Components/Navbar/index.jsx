2e6f4b847fd4882a80b36e98ce15ac8a37d9409d
Rodrigo Mendes <roodrigoomendessilva@gmail.com>
2023-03-01 19:31:33 -0300

    creating part top of navbar

--- econverse/src/Components/Navbar/index.jsx
+++ econverse/src/Components/Navbar/index.jsx
@@ -8,24 +8,22 @@ import './style.sass'
 export function Navbar(){
   return(
     <nav>
-      <div>
-        <div>
+      <div className='top'>
+        <div className='emphasis'>
           <img src={shieldCheck} alt="" />
           <p>compra <span>100% segura</span></p>
         </div>
-        <div>
+        <div className='emphasis'>
           <img src={truck} alt="" />
           <p><span>Frete grátis</span> acima de R$ 200</p>
         </div>
-        <div>
+        <div className='emphasis'>
           <img src={creditCard} alt="" />
           <p><span>Parcele</span> suas Compras</p>
         </div>
-        </div>
-      <div>
-
       </div>
-      <div>
+
+      <div className='middle'>
 
       </div>
     </nav>
