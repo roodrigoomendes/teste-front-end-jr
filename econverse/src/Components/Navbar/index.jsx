e55e32ddc879c6b593a55f357db2cc5acf7aef47
Rodrigo Mendes <roodrigoomendessilva@gmail.com>
2023-03-01 20:56:56 -0300

    details of accessibility

--- econverse/src/Components/Navbar/index.jsx
+++ econverse/src/Components/Navbar/index.jsx
@@ -17,54 +17,54 @@ export function Navbar(){
     <nav>
       <div className='top'>
         <div className='emphasis'>
-          <img src={shieldCheck} alt="" />
+          <img src={shieldCheck} alt="logo shieldCheck" />
           <p>compra <span>100% segura</span></p>
         </div>
         <div className='emphasis'>
-          <img src={truck} alt="" />
+          <img src={truck} alt="logo truck" />
           <p><span>Frete grátis</span> acima de R$ 200</p>
         </div>
         <div className='emphasis'>
-          <img src={creditCard} alt="" />
+          <img src={creditCard} alt="logo creditCard" />
           <p><span>Parcele</span> suas Compras</p>
         </div>
       </div>
 
       <div className='middle'>
 
-        <img src={vtex} alt="" />
+        <img src={vtex} alt="logo vtex" />
         
         <div className='box-search'>
           <input type="search" placeholder='O que você está buscando?' name="search" id="search" />
-          <img src={magnifyingGlass} alt="" />
+          <img src={magnifyingGlass} alt="logo magnifyingGlass" />
         </div>
 
         <div className='box-shortcuts'>
           <a href="">
-            <img src={order} alt="" />
+            <img src={order} alt="logo order" />
           </a>
           <a href="">
-            <img src={heart} alt="" />
+            <img src={heart} alt="logo heart" />
           </a>
           <a href="">
-            <img src={usercircle} alt="" />
+            <img src={usercircle} alt="logo usercircle" />
           </a>
           <a href="">
-            <img src={shoppingCart} alt="" />
+            <img src={shoppingCart} alt="logo shoppingCart" />
           </a>
         </div>
       
       </div>
 
       <div className="bottom">
-        <a href="">todas categorias</a>
-        <a href="">supermercado</a>
-        <a href="">livros</a>
-        <a href="">moda</a>
-        <a href="">lançamentos</a>
-        <a href="" className='active'>oferta do dia</a>
-        <a href="">
-          <img src={crownSimple} alt="" />
+        <a href="#">todas categorias</a>
+        <a href="#">supermercado</a>
+        <a href="#">livros</a>
+        <a href="#">moda</a>
+        <a href="#">lançamentos</a>
+        <a href="#" className='active'>oferta do dia</a>
+        <a href="#">
+          <img src={crownSimple} alt="logo crownSimple" />
           assiantura</a>
       </div>
     </nav>
