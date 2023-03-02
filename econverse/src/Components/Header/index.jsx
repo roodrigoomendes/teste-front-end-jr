37cea5dd28ff9bcf45399617a893c06f2f4f01c7
Rodrigo Mendes <roodrigoomendessilva@gmail.com>
2023-03-02 20:17:48 -0300

    adjusting code organization

--- econverse/src/Components/Header/index.jsx
+++ econverse/src/Components/Header/index.jsx
@@ -3,9 +3,9 @@ import './style.sass'
 
 export function Header(){
   return(
-    <header>
-      <h3>Venha conhecer nossas promoções</h3>
-      <h4>50% Off nos produtos </h4>
+    <header className='banner'>
+      <h3 className='title'>Venha conhecer nossas promoções</h3>
+      <h4 className='subTitle'>50% Off nos produtos </h4>
       <button>Ver produto</button>
 
     </header>
--- econverse/src/Components/Navbar/index.jsx
+++ econverse/src/Components/Navbar/index.jsx
@@ -1,13 +1,13 @@
-import shieldCheck from '../../img/shieldCheck.png'
-import truck from '../../img/truck.png'
-import creditCard from '../../img/CreditCard.png'
-import vtex from '../../img/vtex.png'
-import shoppingCart from '../../img/ShoppingCart.png'
-import heart from '../../img/Heart.png'
-import usercircle from '../../img/UserCircle.png'
-import order from '../../img/order.png'
-import magnifyingGlass from '../../img/MagnifyingGlass.png'
-import crownSimple from '../../img/CrownSimple.png'
+import shieldCheck from '../../assets/Icons/shieldCheck.png'
+import truck from '../../assets/Icons/truck.png'
+import creditCard from '../../assets/Icons/CreditCard.png'
+import vtex from '../../assets/Icons/vtex.png'
+import shoppingCart from '../../assets/Icons/ShoppingCart.png'
+import heart from '../../assets/Icons/Heart.png'
+import usercircle from '../../assets/Icons/UserCircle.png'
+import order from '../../assets/Icons/order.png'
+import magnifyingGlass from '../../assets/Icons/MagnifyingGlass.png'
+import crownSimple from '../../assets/Icons/CrownSimple.png'
 
 
 import './style.sass'
@@ -17,53 +17,53 @@ export function Navbar(){
     <nav>
       <div className='top'>
         <div className='emphasis'>
-          <img src={shieldCheck} alt="logo shieldCheck" />
+          <img src={shieldCheck} alt="Protection" />
           <p>compra <span>100% segura</span></p>
         </div>
         <div className='emphasis'>
-          <img src={truck} alt="logo truck" />
+          <img src={truck} alt="Shipping" />
           <p><span>Frete grátis</span> acima de R$ 200</p>
         </div>
         <div className='emphasis'>
-          <img src={creditCard} alt="logo creditCard" />
+          <img src={creditCard} alt="CreditCard" />
           <p><span>Parcele</span> suas Compras</p>
         </div>
       </div>
 
       <div className='middle'>
 
-        <img src={vtex} alt="logo vtex" />
+        <img src={vtex} alt="vtex" />
         
         <div className='box-search'>
           <input type="search" placeholder='O que você está buscando?' name="search" id="search" />
-          <img src={magnifyingGlass} alt="logo magnifyingGlass" />
+          <img src={magnifyingGlass} alt="Research" />
         </div>
 
         <div className='box-shortcuts'>
           <a href="">
-            <img src={order} alt="logo order" />
+            <img src={order} alt="Order" />
           </a>
           <a href="">
-            <img src={heart} alt="logo heart" />
+            <img src={heart} alt="Favorites" />
           </a>
           <a href="">
-            <img src={usercircle} alt="logo usercircle" />
+            <img src={usercircle} alt="User" />
           </a>
           <a href="">
-            <img src={shoppingCart} alt="logo shoppingCart" />
+            <img src={shoppingCart} alt="ShoppingCart" />
           </a>
         </div>
       
       </div>
 
       <div className="bottom">
-        <a href="#">todas categorias</a>
-        <a href="#">supermercado</a>
-        <a href="#">livros</a>
-        <a href="#">moda</a>
-        <a href="#">lançamentos</a>
-        <a href="#" className='active'>oferta do dia</a>
-        <a href="#">
+        <a className='categories' href="#">todas categorias</a>
+        <a className='categories' href="#">supermercado</a>
+        <a className='categories' href="#">livros</a>
+        <a className='categories' href="#">moda</a>
+        <a className='categories' href="#">lançamentos</a>
+        <a className='categories active' href="#">oferta do dia</a>
+        <a className='categories' href="#">
           <img src={crownSimple} alt="logo crownSimple" />
           assiantura</a>
       </div>
