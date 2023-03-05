2a9d5bba1d5ade1067e95c36a5bca2d8fc5eb879
Rodrigo Mendes <roodrigoomendessilva@gmail.com>
2023-03-05 14:12:34 -0300

    finish footer

--- econverse/src/Components/Footer/index.jsx
+++ econverse/src/Components/Footer/index.jsx
@@ -18,43 +18,46 @@ import { Newsletter } from '../Newsletter'
 export function Footer(){
   return(
     <footer className='footer'>
-      <div className='footerDirection'>
-        <p className='titleFooter'>Sobre nós</p>
-        <a className='optionFooter' href="#">começa</a>
-        <a className='optionFooter' href="#">como comprar</a>
-        <a className='optionFooter' href="#">indicação e desconto</a>
-        <div className='footerLinkSocial'>
-          <img src={facebook} alt="facebook" />
-          <img src={instagram} alt="instagram" />
-          <img src={youtube} alt="youtube" />
+      <div className='footerContent'>
+        <div className='footerDirection'>
+          <p className='titleFooter'>Sobre nós</p>
+          <a className='optionFooter' href="#">começa</a>
+          <a className='optionFooter' href="#">como comprar</a>
+          <a className='optionFooter' href="#">indicação e desconto</a>
+          <div className='footerLinkSocial'>
+            <img src={facebook} alt="facebook" />
+            <img src={instagram} alt="instagram" />
+            <img src={youtube} alt="youtube" />
+          </div>
+        </div>
+        <div className='footerInformation columnCenter footerDirection'>
+          <p className='titleFooter'>informações úteis</p>
+            <a className='optionFooter' href="#">fale conosco</a>
+            <a className='optionFooter' href="#">dúvidas</a>
+            <a className='optionFooter' href="#">prazos de entrega</a>
+            <a className='optionFooter' href="#">formas de pagamento</a>
+            <a className='optionFooter' href="#">política de privacidade</a>
+            <a className='optionFooter' href="#">trocas e devoluções</a>
+        </div>
+        <div className='footerDirection'>
+          <p className='titleFooter'>formas de pagamento</p>
+          <ul className='footerPayments'>
+            <li className='flag'><img src={visa} alt="visa" /></li>
+            <li className='flag'><img src={elo} alt="elo" /></li>
+            <li className='flag'><img src={alelo} alt="alelo" /></li>          
+            <li className='flag'><img src={dinners} alt="dinners" /></li>
+            <li className='flag'><img src={ifood} alt="ifood" /></li>
+            <li className='flag'><img src={mastercard} alt="mastercard" /></li>          
+            <li className='flag'><img src={pix} alt="pix" /></li>
+            <li className='flag'><img src={amex} alt="amex" /></li>
+            <li className='flag'><img src={ticket} alt="ticket" /></li>          
+            <li className='flag'><img src={sodexo} alt="sodexo" /></li>
+            
+          </ul>        
         </div>
       </div>
-      <div className='footerInformation columnCenter footerDirection'>
-        <p className='titleFooter'>informações úteis</p>
-          <a className='optionFooter' href="#">fale conosco</a>
-          <a className='optionFooter' href="#">dúvidas</a>
-          <a className='optionFooter' href="#">prazos de entrega</a>
-          <a className='optionFooter' href="#">formas de pagamento</a>
-          <a className='optionFooter' href="#">política de privacidade</a>
-          <a className='optionFooter' href="#">trocas e devoluções</a>
-      </div>
-      <div className='footerDirection'>
-        <p className='titleFooter'>formas de pagamento</p>
-        <ul className='footerPayments'>
-          <li className='flag'><img src={visa} alt="visa" /></li>
-          <li className='flag'><img src={elo} alt="elo" /></li>
-          <li className='flag'><img src={alelo} alt="alelo" /></li>          
-          <li className='flag'><img src={dinners} alt="dinners" /></li>
-          <li className='flag'><img src={ifood} alt="ifood" /></li>
-          <li className='flag'><img src={mastercard} alt="mastercard" /></li>          
-          <li className='flag'><img src={pix} alt="pix" /></li>
-          <li className='flag'><img src={amex} alt="amex" /></li>
-          <li className='flag'><img src={ticket} alt="ticket" /></li>          
-          <li className='flag'><img src={sodexo} alt="sodexo" /></li>
-          
-        </ul>        
-      </div>
       <Newsletter/>
+
     </footer>
   )
 }
